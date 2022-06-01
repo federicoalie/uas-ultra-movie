37d8cd6bdb12e38923fe0cc067306b9bd32ef3d8
federicoalie11 <federicoalie11@gmail.com>
2022-06-01 10:33:00 +0700

    menambahkan dan mendesain login page, lalu menambahkan dan membetulkan beberapa bug

---
+++ app/src/main/java/com/gdz/ultramovie/loginActivity.java
@@ -0,0 +1,19 @@
+package com.gdz.ultramovie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void btnSinUp(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/gdz/ultramovie/splashScreen.java
@@ -0,0 +1,27 @@
+package com.gdz.ultramovie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashScreen extends AppCompatActivity {
+
+    private static final int splash_timeout = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), loginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, splash_timeout);
+    }
+}
