2619fe0ad453ea38f442b11ed09f36a2c1411c84
federicoalie11 <federicoalie11@gmail.com>
2022-06-03 11:39:40 +0700

    membuat dan mendesain main menu untuk admin. Menambahkan implementation gradle baru

---
+++ app/src/main/java/com/gdz/ultramovie/mainMenuAdmin.java
@@ -0,0 +1,25 @@
+package com.gdz.ultramovie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class mainMenuAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu_admin);
+    }
+
+    public void btnAboutUs(View view) {
+        Intent aboutUs = new Intent(getApplicationContext(), aboutUsActivity.class);
+        startActivity(aboutUs);
+    }
+
+    public void btnProfile(View view) {
+
+    }
+}
